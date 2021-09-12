6f2df26c9b7a87a1e43777c5ab2a7a6053259a82
Filip Glura <lockontargt@gmail.com>
2021-09-12 22:34:03 +0200

    feat: added header

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import styles from '../styles/Home.module.scss';
 import { useState } from 'react';
 import { Heading } from 'components/atoms/Heading/Heading';
+import { Button } from 'components/atoms/Button/Button';
 import { Chat } from 'components/molecules/Chat/Chat';
 
 export default function Home() {
     const [user, setUser] = useState(false);
   return (
-    <div className={styles.container}>
+    <div className={styles.home}>
         <Heading>chat-app</Heading>
-        <Chat />
+        <div className={styles.wrapper}>
+            <div className={styles.header}>
+                <Button>Sign out</Button>
+            </div>
+            <Chat />
+        </div>
     </div>
   )
 }
