fd334ff98a5f06f9b1bdea59b6641c26c008a29d
Filip Glura <lockontargt@gmail.com>
2021-09-13 10:41:28 +0200

    feat: added supabase client and google OAuth sign in

---
+++ client.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    "https://pejryzqentklhwacybfx.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMTQ2OTEyMCwiZXhwIjoxOTQ3MDQ1MTIwfQ.DMCXTp02Iwiomfn_PJOGg7jfDxj7_fDC-gO4FZszo1k"
+);
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import styles from "../styles/Home.module.scss";
 import { useState } from "react";
+import { supabase } from "client.js";
 import { Heading } from "components/atoms/Heading/Heading";
 import { Button } from "components/atoms/Button/Button";
 import { Chat } from "components/molecules/Chat/Chat";
 
 export default function Home() {
     const [user, setUser] = useState(false);
+
+    async function signInWithGoogle() {
+        const { user, session, error } = await supabase.auth.signIn({
+            provider: "google",
+        });
+    }
     return (
         <div className={styles.home}>
             <Heading>chat-app</Heading>
@@ -18,9 +25,7 @@ export default function Home() {
                 </div>
             ) : (
                 <div className={styles.signIn}>
-                    <Button
-                        onClick={() => setUser(true)}
-                    >
+                    <Button onClick={() => { setUser(true); signInWithGoogle(); }}>
                         Sign In with google
                     </Button>
                 </div>
