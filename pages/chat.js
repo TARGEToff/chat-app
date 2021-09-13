ca9a9176df0611d27d10f68408dba4790d3c5518
Filip Glura <lockontargt@gmail.com>
2021-09-13 11:04:42 +0200

    feat: added chat page, deleted Chat component

---
+++ pages/chat.js
@@ -0,0 +1,30 @@
+import styles from "../styles/Home.module.scss";
+import Link from "next/link"
+import { supabase } from "client.js";
+import { Heading } from "components/atoms/Heading/Heading";
+import { Button } from "components/atoms/Button/Button";
+
+export default function Chat() {
+
+    async function signout() {
+        const { error } = await supabase.auth.signOut();
+    }
+
+    return (
+        <div className={styles.home}>
+            <Heading>chat-app</Heading>
+            <div className={styles.wrapper}>
+                <div className={styles.header}>
+                    <Link href="/">
+                        <Button onClick={() => signout()}>Sign out</Button>
+                    </Link>
+                </div>
+                <div className={styles.chat}></div>
+                <form className={styles.form}>
+                    <input className={styles.formInput} />
+                    <Button type="submit">Send</Button>
+                </form>
+            </div>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,25 @@
 import styles from "../styles/Home.module.scss";
-import { useState } from "react";
 import { supabase } from "client.js";
 import { Heading } from "components/atoms/Heading/Heading";
 import { Button } from "components/atoms/Button/Button";
-import { Chat } from "components/molecules/Chat/Chat";
 
 export default function Home() {
-    const [user, setUser] = useState(false);
 
     async function signInWithGoogle() {
         const { user, session, error } = await supabase.auth.signIn({
             provider: "google",
+        }, {
+            redirectTo: "https://localhost:3000/chat",
         });
     }
     return (
         <div className={styles.home}>
             <Heading>chat-app</Heading>
-            {user ? (
-                <div className={styles.wrapper}>
-                    <div className={styles.header}>
-                        <Button onClick={() => setUser(false)}>Sign out</Button>
-                    </div>
-                    <Chat />
-                </div>
-            ) : (
                 <div className={styles.signIn}>
-                    <Button onClick={() => { setUser(true); signInWithGoogle(); }}>
+                    <Button onClick={() => signInWithGoogle()}>
                         Sign In with google
                     </Button>
                 </div>
-            )}
         </div>
     );
 }
